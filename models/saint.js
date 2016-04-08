12a43c3b91e34eeef99301c27b046685062ce12f
cloverharvest <anna@schwabdev.com>
2016-04-08 12:00:07 -0700

    Adds file structure, schemas, index.html

---
+++ models/saint.js
@@ -0,0 +1,17 @@
+//models/saints.js
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+var DedicatedChurch = require('./dedicatedchurch');
+​
+var SaintSchema = new Schema ({
+  name: String,
+  patronSaintOf: String,
+  feastDate: String,
+  birthplace: String,
+  funFact: String,
+  imageUrl: String,
+  dedicatedChurches : [DedicatedChurch.schema]
+});
+​
+var Saint = mongoose.model('Saint', SaintSchema);
+module.exports = Saint;
