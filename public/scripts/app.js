b7b9567faf199439a58e9e9fa5363c1f55ee9c10
cloverharvest <anna@schwabdev.com>
2016-04-11 20:48:00 -0700

    Displays buttons, builds function for opening field and allowing changes in saint proeprties data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
   //ajax call to get the hardcoded-saints in saintsController.js
   $.get('/api/saints').success(function (saints) {
     saints.forEach(function(saint) {
-    console.log("saint", saint);
       renderSaint(saint);
     });
   });
@@ -44,12 +43,15 @@ $(document).ready(function() {
     });
     //save a new church
     $('#saveChurch').on('click', handleNewChurchSubmit);
-
-    //edit a church
-    $('#saints').on('click', '.edit-church', handleChurchEditClick);
-
+    //
     //delete a saint
     $('#saints').on('click', '.delete-saint', handleDeleteSaintClick);
+    //edit a saint
+    $('#saints').on('click', '.edit-saint', handleSaintEditClick);
+    //save the changes to a saint
+    $('#saints').on('click', '.edit-saint', handleSaveSaintChangesClick);
+
+
   });
 
 
@@ -67,12 +69,44 @@ function renderSaint(saint) {
 
 /////////////////////////////
 
-//EDIT A CHURCH ===> this is an additional feature to be worked on
+//EDIT A SAINT'S FUNFACT AND PATRON SAINT OF ===> this is an additional feature to be worked on
+
+//when the edit button for the a saint is clicked this function is executed
+function handleSaintEditClick(evt) {
+  // var saintId = $(this).closest('.saint').data('saint-id');
+  var $saintRow = $(this).closest('.saint');
+  console.log("$saintRow: ", $saintRow);
+  var saintId = $saintRow.data('saint-id');
+  console.log("edit saint: ", saintId);
 
 
-function handleChurchEditClick(evt) {
-  var saintId = $(this).closest('.saint').data('saint-id');
-  console.log('edit saint', saintId);
+//show the save changes button
+$saintRow.find('.save-saint').toggleClass('hidden');
+//then hide the edit button
+$saintRow.find('.edit-saint').toggleClass('hidden');
+
+//opens the patron-name field and allows user to replace it  with new data
+var saintPatronOf = $saintRow.find('span.patron-name').text();
+$saintRow.find('span.patron-name').html('<input class="patron-name" value="' + saintPatronOf + '"></input>');
+
+//opens the fun fact details field and allows user to replace it with a new data
+var saintFunFact = $saintRow.find('span.funfact-details').text();
+$saintRow.find('span.funfact-details').html('<input class="funfact-details" value="' + saintFunFact + '"></input>');
+
+}
+
+//after changes/edits have been made this save function is run
+function handleSaveSaintChangesClick(evt) {
+  var saintId = $(this).parents('.saint').data('saint-id');
+  console.log("saintId: ", saintId);
+
+  var $saintRow = $('[data-saint-id=' + saintId + ']');
+  console.log("$saintRow: ", $saintRow);
+
+  //save the new input in the patron-name field
+
+  //save the new input in the fun-fact field
+
 }
 
 /////////////////////////////
